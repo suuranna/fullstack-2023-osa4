af412d4f92150f7c9c140e22271f5bc8015594b8
Anna Suuronen <annasuuronen@icloud.com>
2023-09-14 17:51:09 +0300

    tehtävä 4.2 tehty

---
+++ app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+const Blog = require('./models/blog')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
