b7051410c824199db3e38c248c2e204d5d34f387
Anna Suuronen <annasuuronen@icloud.com>
2023-09-14 21:41:12 +0300

    tehtävä 4.4 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
   return 1
 }
-  
+
+const totalLikes = (blogs) => {
+	var total = blogs.reduce(function(sum, blog) {
+		return sum + blog.likes
+	}, 0)
+
+	return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+	totalLikes
 }
