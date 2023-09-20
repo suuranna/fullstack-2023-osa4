b518a05f8bb7143cb753071b2a5ab40458484cab
Anna Suuronen <annasuuronen@icloud.com>
2023-09-20 15:02:23 +0300

    tehtävä 4.19 tehty

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.startsWith('Bearer ')) {
+	  return authorization.replace('Bearer ', '')
+	}
+	return null
+}
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { name: 1, username: 1, id: 1 })
@@ -10,8 +20,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   	const body = request.body
 
-	const users = await User.find({ 'name': /^M/})
-	const user = users[0]
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  	if (!decodedToken.id) {
+		return response.status(401).json({ error: 'token invalid' })
+	}
+	
+	const user = await User.findById(decodedToken.id)
 
 	const blog = new Blog({
 		title: body.title,
--- utils/config.js
+++ utils/config.js
@@ -6,9 +6,12 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
 	? process.env.TEST_MONGODB_URI
 	: process.env.MONGODB_URI
 
+const SECRET = process.env.SECRET
+
 //let MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SECRET
 }
