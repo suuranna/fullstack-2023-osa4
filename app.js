afbcb32799b64887b0296d3a0aaffb9da7804b44
Anna Suuronen <annasuuronen@icloud.com>
2023-09-20 13:19:42 +0300

    tehtävä 4.18 tehty

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -18,5 +19,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
